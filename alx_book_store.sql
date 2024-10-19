be8005817abfb443a2b111bed8a684ef5d8d04b1
Nebyou Damtew <nebyoudamtew18@gmail.com>
2024-10-19 18:26:50 +0300

    a MySQL database for an online bookstore.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -32,7 +32,7 @@ CREATE TABLE Orders(
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE
-    FOREIGN KEY customer_id REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 );
 
 --Stores information about the books included in each order.
@@ -40,6 +40,6 @@ CREATE TABLE Order_Details(
     order_detail_id INT PRIMARY KEY,
     order_id INT,  
     book_id INT, 
-    FOREIGN KEY order_id REFERENCES Orders(order_id),
-    FOREIGN KEY book_id REFERENCES Books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
